b1738b3e368e37a6828cbb1be574cfaecd13ce92
tanguydm@gmail.com <tanguydm@gmail.com@bc18b99c-8f34-b30d-8ea6-c7fc415a0156>
2011-03-07 20:34:23 +0000

    Tag de la Beta 0.5.1.0

---
+++ tags/0.5.1.0/DAL/WriteDB.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Data.SQLite;
+using System.Windows.Forms;
+using TaskLeader.BO;
+using TaskLeader.GUI;
+
+namespace TaskLeader.DAL
+{
+    public class WriteDB
+    {
+        // Variable locale pour stocker une référence vers l'instance
+        private static WriteDB instance = null;
+
+        // Renvoie l'instance ou la crée
+        public static WriteDB Instance
+        {
+            get
+            {
+                // Si pas d'instance existante on en crée une...
+                if (instance == null)
+                    instance = new WriteDB();
+
+                // On retourne l'instance de MonSingleton
+                return instance;
+            }
+        }
+
+        // Méthode générique pour exécuter une requête sql fournie en paramètre, retourne le nombre de lignes modifiées
+        public int execSQL(String requete)
+        {
+            // Si le mode debug est activé, on affiche les requêtes
+            if (System.Configuration.ConfigurationManager.AppSettings["debugMode"] == "true")
+                MessageBox.Show(requete, "Requête", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            try
+            {
+                using (SQLiteCommand SQLCmd = new SQLiteCommand(ConnexionDB.Instance.getConnection()))
+                {
+                    // Création d'une nouvelle commande à partir de la connexion
+                    SQLCmd.CommandText = requete;
+                    //Exécution de la commande
+                    return SQLCmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception Ex)
+            {
+                // On affiche l'erreur.
+                MessageBox.Show(Ex.Message, "Exception sur execSQL", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return -1;
+            }
+        }
+
+        // Insertion en base d'un nouveau filtre
+        public void insertFiltre(String titre, Object[] criteria)
+        {
+            using (SQLiteTransaction mytransaction = ConnexionDB.Instance.getConnection().BeginTransaction())
+            {
+                using (SQLiteCommand SQLCmd = new SQLiteCommand(ConnexionDB.Instance.getConnection()))
+                {
+                    // On insère le nom du filtre
+                    String nomFiltre = "'" + titre.Replace("'", "''") + "'"; // Le titre du filtre ne doit pas contenir de quote
+                    SQLCmd.CommandText = "INSERT INTO Filtres (Titre) VALUES ("+nomFiltre+");";
+                    SQLCmd.ExecuteNonQuery();
+
+                    // On insère ensuite dans les tables annexes les données sélectionnées
+                    foreach (Criterium critere in criteria)
+                    {
+                        String selection;
+
+                        // On crée la requête pour insertion des critères dans les tables annexes
+                        String requete = "INSERT INTO " + ConnexionDB.Instance.schema[critere.champ, 3];
+                        requete += " VALUES ((SELECT max(rowid) FROM Filtres),"; // On récupère le rowid du filtre frâichement créé
+                        requete += "(SELECT rowid FROM " + ConnexionDB.Instance.schema[critere.champ, 1];
+                        requete += " WHERE " + ConnexionDB.Instance.schema[critere.champ, 2] + "=@Titre));";
+
+                        SQLCmd.CommandText = requete;
+                        SQLiteParameter p_Titre = new SQLiteParameter("@Titre");
+                        SQLCmd.Parameters.Add(p_Titre);
+
+                        foreach (String item in critere.selected)
+                        {
+                            selection = item.Replace("'", "''"); // On gère les simple quote
+                            p_Titre.Value = selection;
+                            SQLCmd.ExecuteNonQuery();
+                        }
+
+                        // On précise que la case ALL n'a pas été sélectionnée pour ce critère
+                        SQLCmd.CommandText = "UPDATE Filtres SET " + ConnexionDB.Instance.schema[critere.champ, 4] + "=0 WHERE rowid = (SELECT max(rowid) FROM Filtres)";
+                        SQLCmd.ExecuteNonQuery();
+                    }
+                }
+                mytransaction.Commit();
+            }
+
+            // On affiche un message de statut sur la TrayIcon
+            TrayIcon.afficheMessage("Bilan création/modification","Nouveau filtre ajouté: "+titre);
+
+        }
+
+        // Insertion d'un nouveau contexte en base
+        public int insertContexte(String contexte)
+        {
+            String ctxt = "'" + contexte.Replace("'", "''") + "'";
+            String requete = "INSERT INTO Contextes (Titre) VALUES (" + ctxt + ")";
+            return execSQL(requete);
+        }
+
+        // Insertion d'un nouveau sujet en base
+        public int insertSujet(String contexte, String subject)
+        {
+            String ctxt = "'" + contexte.Replace("'", "''") + "'";
+            String sujet = "'" + subject.Replace("'", "''") + "'";
+
+            String requete = "INSERT INTO Sujets (CtxtID,Titre)" +
+                            " SELECT C.rowid, " + sujet +
+                            " FROM Contextes C" +
+                            " WHERE C.Titre = " + ctxt;
+
+            return execSQL(requete);
+        }
+
+        // Insertion d'un nouveau destinataire en base
+        public int insertDest(String destinataire)
+        {
+            String dest = "'" + destinataire.Replace("'", "''") + "'";
+
+            String requete = "INSERT INTO Destinataires (Nom) VALUES (" + dest + ")";
+
+            return execSQL(requete);
+        }
+    
+        // Insertion d'une nouvelle action
+        public int insertAction(String contexte, String subject, String desAction, String dueDate, String destinataire, String stat)
+        {
+            //Syntaxe: INSERT INTO Actions (nom des colonnes avec ,) VALUES(valeurs avec ' et ,)     
+
+            // Préparation des différents morceaux de la requête
+            String insertPart = "INSERT INTO Actions (";
+            String valuePart = " VALUES (";
+            String ctxt;
+
+            if (contexte != "")
+            {
+                ctxt = "'" + contexte.Replace("'", "''") + "'";
+                insertPart += "CtxtID,";
+                valuePart += "(SELECT rowid FROM Contextes WHERE Titre = "+ctxt+"),";
+            }
+
+            if (subject != "")
+            {
+                ctxt = "'" + contexte.Replace("'", "''") + "'";
+                String sujet = "'" + subject.Replace("'", "''") + "'";
+                insertPart += "SujtID,";
+                valuePart += "(SELECT id FROM VueSujets WHERE Contexte="+ctxt+" AND Titre="+sujet+"),";
+            }
+
+            insertPart += "Titre,";
+            valuePart += "'" + desAction.Replace("'", "''") + "',";
+
+            if (dueDate != "")
+            {
+                insertPart += "DueDate,";
+                valuePart += "'" + dueDate + "',";
+            }
+
+            if (destinataire != "")
+            {
+                String dest = "'" + destinataire.Replace("'", "''") + "'";
+                insertPart += "DestID,";
+                valuePart += "(SELECT rowid FROM Destinataires WHERE Nom ="+dest+"),";
+            }
+
+            insertPart += "IDMail,";
+            valuePart += "'N/A',"; // Pour l'instant l'IDmail n'est pas géré
+
+            String statut = "'" + stat.Replace("'", "''") + "'";
+            insertPart += "StatID)";
+            valuePart += "(SELECT rowid FROM Statuts WHERE Titre="+statut+"))";
+
+            return execSQL(insertPart + valuePart);
+        }
+
+        // Mise à jour d'une action
+        public int updateAction(String contexte, String subject, String desAction, String dueDate, String destinataire, String stat, String id)
+        {
+            // Préparation des sous requêtes
+            String ctxtPart = "(SELECT rowid FROM Contextes WHERE Titre=" + "'" + contexte.Replace("'", "''") + "')";
+            String sujetPart = "(SELECT rowid FROM Sujets WHERE Titre=" + "'" + subject.Replace("'", "''") + "')";
+            String action = "'" + desAction.Replace("'", "''") + "'";
+            String date = "'" + dueDate + "'";
+            String destPart = "(SELECT rowid FROM Destinataires WHERE Nom=" + "'" + destinataire.Replace("'", "''") + "')";
+            String statPart = "(SELECT rowid FROM Statuts WHERE Titre=" + "'" + stat.Replace("'", "''") + "')";
+
+            String requete = "UPDATE Actions SET CtxtID=" + ctxtPart + ",SujtID=" + sujetPart + ",Titre=" + action + "," +
+                "DueDate=" + date + ",DestID=" + destPart + ",StatID=" + statPart + " WHERE rowid='" + id + "'";
+
+            return execSQL(requete);
+        }
+
+    }
+}
---
+++ tags/0.5.1.0/GUI/TrayIcon.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using TaskLeader.BLL;
+
+namespace TaskLeader.GUI
+{
+    public class TrayIcon: ApplicationContext
+    {
+
+        private static NotifyIcon trayIcon = new NotifyIcon();
+        private ContextMenuStrip trayContext = new ContextMenuStrip();
+        private ToolStripMenuItem newActionItem = new ToolStripMenuItem();
+        private ToolStripMenuItem closeItem = new ToolStripMenuItem();
+        private ToolStripMenuItem maximItem = new ToolStripMenuItem();
+
+        // Déclaration de tous les composants
+        private void loadComponents()
+        {
+            // trayIcon
+            trayIcon.ContextMenuStrip = this.trayContext;
+            trayIcon.Icon = Properties.Resources.task_coach;
+            trayIcon.Text = "TaskLeader";
+            trayIcon.Visible = true;
+            trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+
+            // Menu contextuel de la trayIcon
+            this.trayContext.Items.AddRange(new ToolStripItem[] { this.newActionItem, this.maximItem, this.closeItem });
+            this.trayContext.Name = "trayContext";
+            this.trayContext.ShowImageMargin = false;
+            this.trayContext.Size = new System.Drawing.Size(126, 70);
+
+            // Item "nouvelle action" du menu contextuel
+            this.newActionItem.Name = "newActionItem";
+            this.newActionItem.ShowShortcutKeys = false;
+            this.newActionItem.Size = new System.Drawing.Size(125, 22);
+            this.newActionItem.Text = "Nouvelle action";
+            this.newActionItem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.newActionItem.Click += new System.EventHandler(this.ajoutAction);
+
+            // Item "afficher Toolbox" du menu contextuel
+            this.maximItem.Name = "maximItem";
+            this.maximItem.Size = new System.Drawing.Size(125, 22);
+            this.maximItem.Text = "Afficher toolbox";
+            this.maximItem.Click += new System.EventHandler(this.maximItem_Click);
+
+            // Item "fermer" du menu contextuel
+            this.closeItem.Name = "closeItem";
+            this.closeItem.Size = new System.Drawing.Size(125, 22);
+            this.closeItem.Text = "Fermer";
+            this.closeItem.Click += new System.EventHandler(this.closeItem_Click);
+        }
+
+        // Constructeur de la NotifyIcon
+        public TrayIcon()
+        {
+            // On charge tous les composants
+            this.loadComponents();
+
+            // Vérification de démarrage
+            if (Init.Instance.canLaunch())
+                this.displayToolbox(); // Affichage de la Toolbox
+            else
+                this.closeApp(); // On ferme l'appli
+        }
+
+        private Toolbox v_toolbox = null;
+
+        // Méthode générique d'affichage de la Toolbox
+        private void displayToolbox()
+        {
+            if (v_toolbox == null || v_toolbox.IsDisposed) // Si la fenêtre n'a jamais été ouverte ou fermée, on l'ouvre
+            {
+                v_toolbox = new Toolbox();
+                v_toolbox.WindowState = FormWindowState.Maximized;
+                v_toolbox.Show();
+            }
+            else
+                v_toolbox.BringToFront(); // Sinon on l'affiche au premier plan     
+        }
+
+        // Double-click sur la trayIcon
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.displayToolbox();
+        }
+
+        // Ajout d'action
+        private void ajoutAction(object sender, EventArgs e)
+        {
+            ManipAction fenetre = new ManipAction();
+            fenetre.Show();
+        }
+
+        // Demande d'affichage de la Toolbox via le ContextMenuStrip
+        private void maximItem_Click(object sender, EventArgs e)
+        {
+            this.displayToolbox();
+        }
+
+        // Méthode générique de fermeture de l'appli
+        private void closeApp()
+        {
+            trayIcon.Visible = false; 
+            Application.Exit();
+        }
+
+        // Demande de fermeture via le ContextMenuStrip
+        private void closeItem_Click(object sender, EventArgs e)
+        {
+            this.closeApp();
+        }
+
+        // Méthode pour affichage de message
+        public static void afficheMessage(String titre, String info)
+        {
+            // Récupération de la durée d'affichage de la tooltip
+            int duree = int.Parse(ConfigurationManager.AppSettings["dureeTooltip"]);
+
+            // Affichage du bilan en tooltip de la tray icon
+            trayIcon.ShowBalloonTip(duree, titre, info, ToolTipIcon.Info);
+        }
+    }
+}
