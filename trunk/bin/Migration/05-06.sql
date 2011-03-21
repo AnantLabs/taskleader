b51f93d4e3468138dc4d2bbce3668783fb443507
tanguydm@gmail.com <tanguydm@gmail.com@bc18b99c-8f34-b30d-8ea6-c7fc415a0156>
2011-03-21 11:13:57 +0000

    Finalement la vue sera harcodée, update du script de migration.

--- trunk/bin/Migration/05-06.sql
+++ trunk/bin/Migration/05-06.sql
@@ -6,9 +6,22 @@ UPDATE Actions SET IDMail=NULL WHERE IDMail IS NOT NULL;
 
 CREATE TABLE [Mails] ([StoreID] VARCHAR NOT NULL, [EntryID] VARCHAR NOT NULL, [MessageID] VARCHAR NOT NULL);
 
-/* 3. Suppression de vueActions. Etant fortement liée à l'IHM, la vue sera créée par l'appli */
+/* 3. Mise à jour de la vueActions */
 
 DROP VIEW VueActions;
+
+CREATE VIEW VueActions AS
+   SELECT A.rowid as 'id', C.Titre as 'Contexte', Su.Titre as 'Sujet', A.Titre, A.IDMail as 'Mail', strftime("%d-%m-%Y",A.DueDate) as 'Deadline', D.Nom as 'Destinataire', A.IDMail, St.Titre as 'Statut'
+   FROM Actions A
+      LEFT OUTER JOIN Contextes C
+         ON A.CtxtID = C.rowid
+      LEFT OUTER JOIN Sujets Su
+         ON  A.SujtID = Su.rowid
+      LEFT OUTER JOIN Destinataires D
+         ON  A.DestID = D.rowid
+      LEFT OUTER JOIN Statuts St
+         ON  A.StatID = St.rowid
+   ORDER BY DueDate ASC;
 
 /* 4. Mise à jour de la table VerComp */
 
