745bc49ba6db2dbe9aa7368d46ba0042d092a1a1
tanguydm@gmail.com <tanguydm@gmail.com@bc18b99c-8f34-b30d-8ea6-c7fc415a0156>
2011-03-21 10:51:55 +0000

    Mise à jour du script de migration 05-06

--- bin/Migration/05-06.sql
+++ bin/Migration/05-06.sql
@@ -1,4 +1,4 @@
-﻿/* 1. Suppression des valeurs de la colonne IDMail */
+ /* 1. Suppression des valeurs de la colonne IDMail */
 
 UPDATE Actions SET IDMail=NULL WHERE IDMail IS NOT NULL; 
 
@@ -6,7 +6,11 @@ UPDATE Actions SET IDMail=NULL WHERE IDMail IS NOT NULL;
 
 CREATE TABLE [Mails] ([StoreID] VARCHAR NOT NULL, [EntryID] VARCHAR NOT NULL, [MessageID] VARCHAR NOT NULL);
 
-/* 3. Mise à jour de la table VerComp */
+/* 3. Suppression de vueActions. Etant fortement liée à l'IHM, la vue sera créée par l'appli */
+
+DROP VIEW VueActions;
+
+/* 4. Mise à jour de la table VerComp */
 
 -- Supprimer toutes les entrées (pas de rétrocompatibilité)
 DELETE FROM VerComp;
